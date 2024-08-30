2eb54ea363d2e3df34aa68f1c20d8fbea040e74e
Jared Stanbrook <Jared.Stanbrook@outlook.com>
2024-08-30 22:16:27 +0800

    On branch main

---
+++ schema.sql
@@ -0,0 +1,11 @@
+-- npx wrangler d1 execute it-service-desk-db --remote --command "SELECT students.* FROM students"
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE IF NOT EXISTS students (
+    _id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    student_id VARCHAR(255) NOT NULL
+);
+
+INSERT INTO students (name, student_id) VALUES 
+('Jared Stanbrook','34113043');
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import { Hono } from "hono";
 import { renderer } from "./renderer";
+import { findAllStudents } from "./db";
 
 type Bindings = {
-    MY_DB: D1Database;
+    DB: D1Database;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("*", renderer);
 
-app.get("/", (c) => {
-    return c.render(<h1>Hello, Cloudflare Pages!</h1>);
+app.get("/", async(c) => {
+    const students = await findAllStudents(c.env.DB);
+    return c.render(<h1>Hello, Cloudflare Pages! = {JSON.stringify(students)}</h1>);
 });
 
 export default app;
